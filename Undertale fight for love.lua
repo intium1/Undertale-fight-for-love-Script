74fa292f9d77d7f3cbd74621d398eb6aa0e6656b
intium1 <emirsuyunov.228@xmail.ru>
2024-07-28 11:00:38 +0300

    Update Undertale fight for love.lua

--- Undertale fight for love.lua
+++ Undertale fight for love.lua
@@ -2,7 +2,7 @@ local Library = loadstring(game:HttpGet("https://github.com/intium1/Undertale-fi
 --[[ 
 В данный момент стоит тема "RJTheme2" ,
 вы можете использовать другую тему приведённую ниже -
-"RJTheme1"
+"RJTheme1"Cancel changes
 "RJTheme2"
 "RJTheme3"
 "RJTheme4"
@@ -16,7 +16,20 @@ local Library = loadstring(game:HttpGet("https://github.com/intium1/Undertale-fi
 который находится чуть ниже , и вместо "RJTheme2" в переменной "Windows" - 
 напишите переменную которая используется в скрипте чуть ниже .
 ]]
-
+--[[
+local colors = {
+	-- Цвет фона у Секций
+    SchemeColor = Color3.fromRGB(150, 72, 148),
+	-- Цвет фона в правой части UI
+	Background = Color3.fromRGB(15,15,15),
+	-- Цвет фона в левой части UI
+    Header = Color3.fromRGB(15,15,15),
+	-- Цвет текста
+    TextColor = Color3.fromRGB(255,255,255),
+	-- Цвет фона у кнопок
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+]]
 -- Создать окно UI
 local Window = Library.CreateLib("Undertale fight for love", "RJTheme2")
 
