706d62eb17015a8ca3a5b1508da0f38f7c9c23ef
intium1 <emirsuyunov.228@xmail.ru>
2024-07-29 03:22:01 +0300

    Update Undertale_fight_for_love.lua

--- Undertale_fight_for_love.lua
+++ Undertale_fight_for_love.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 --[[ 
-В данный момент стоит тема "RJTheme3" ,
+В данный момент стоит тема "RJTheme8" ,
 вы можете использовать другую тему приведённую ниже -
 "RJTheme1"
 "RJTheme2"
@@ -13,25 +13,12 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Roboj
 //////////////////////////////////////////////////////////////////
 
 Что бы сделать свою тему , уберите часть скрипта из "комминтариев" ,
-который находится чуть ниже , и вместо "RJTheme3" в переменной "Windows" - 
+который находится чуть ниже , и вместо "RJTheme8" в переменной "Windows" - 
 напишите переменную которая используется в скрипте чуть ниже .
 ]]
---[[
-local colors = {
-	-- Цвет фона у Секций
-    SchemeColor = Color3.fromRGB(150, 72, 148),
-	-- Цвет фона в правой части UI
-	Background = Color3.fromRGB(15,15,15),
-	-- Цвет фона в левой части UI
-    Header = Color3.fromRGB(15,15,15),
-	-- Цвет текста
-    TextColor = Color3.fromRGB(255,255,255),
-	-- Цвет фона у кнопок
-    ElementColor = Color3.fromRGB(20, 20, 20)
-}
-]]
+
 -- Создать окно UI
-local Window = Library.CreateLib("Undertale fight for love", "RJTheme3")
+local Window = Library.CreateLib("Undertale fight for love", "RJTheme8")
 
 -- Секция
 local Tab = Window:NewTab("Reapertale Sans")
