ec8b788f39c32b34710af1346996de54f99bc553
intium1 <emirsuyunov.228@xmail.ru>
2024-07-29 03:24:56 +0300

    Update Undertale_fight_for_love.lua

--- Undertale_fight_for_love.lua
+++ Undertale_fight_for_love.lua
@@ -16,7 +16,20 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Roboj
 который находится чуть ниже , и вместо "RJTheme8" в переменной "Windows" - 
 напишите переменную которая используется в скрипте чуть ниже .
 ]]
-
+--[[
+local colors = {
+	-- Цвет фона у Секций
+    SchemeColor = Color3.fromRGB(150, 72, 148),
+	-- Цвет фона в правой части UI
+	Background = Color3.fromRGB(15,15,15),
+	-- Цвет фона в левой части UI
+    Header = Color3.fromRGB(15,15,15),
+	-- Цвет текста
+    TextColor = Color3.fromRGB(255,255,255),
+	-- Цвет фона у кнопок
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+]]
 -- Создать окно UI
 local Window = Library.CreateLib("Undertale fight for love", "RJTheme8")
 
